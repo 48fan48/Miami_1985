3b3f050e330543b3e2284b3bb0d6b2d732e133d4
Logan Reed Bland <lrb0023@mix.wvu.edu>
2020-10-28 18:52:52 +0000

    Added enemy script.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+/*     public GameObject player; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the player object
+        player = GameObject.Find("Fidel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the distance between the enemy and the player
+        float distance = transform.position - player.transform.position;
+
+        // If the player is within a certain range
+        if(distance < 10) {
+            AttackPlayer();
+        }
+    }
+
+    void AttackPlayer() 
+    {
+        //Make the enemy look at the player
+        transform.LookAt(player.transform);
+    } */
+}
