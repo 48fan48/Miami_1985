72b739d3bb9c78dc7a1b71783678fb108653e29b
Logan Reed Bland <lrb0023@mix.wvu.edu>
2020-10-29 02:35:47 +0000

    Basic enemy shooting when player is within range.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿/*
+ * Unity AI shooting the Player // Top Down 3D Tutorial Beginner 05 by L_Sin Gularity
+ * Logan Bland, John Clay, Jalen Wayt
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+public GameObject player;
+public GameObject projectilePrefab;
 
-/*     public GameObject player; 
+private float timerShots;
+public float timeBtwShots = 0.25f;
+
+public float fireRadius = 5f;
+public float force = 2000f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get the player object
         player = GameObject.Find("Fidel");
     }
 
@@ -18,17 +28,31 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         // Get the distance between the enemy and the player
-        float distance = transform.position - player.transform.position;
+        float distance = Vector3.Distance(player.transform.position, transform.position);
 
         // If the player is within a certain range
-        if(distance < 10) {
-            AttackPlayer();
-        }
+        if(distance <= fireRadius) {
+            ShootPlayer();
+        } 
     }
 
-    void AttackPlayer() 
+    void ShootPlayer() 
     {
-        //Make the enemy look at the player
-        transform.LookAt(player.transform);
-    } */
+        RaycastHit hitPlayer;
+        Ray playerPos = new Ray(transform.position, transform.forward);
+
+        if(Physics.SphereCast(playerPos, 0.25f, out hitPlayer, fireRadius))
+        {
+            if (timerShots <= 0 && hitPlayer.transform.tag == "Player")
+            {
+                Instantiate(projectilePrefab, transform.position + new Vector3(1,0,0), transform.rotation);
+                timerShots = timeBtwShots;
+            }
+            else 
+            {
+                timerShots -= Time.deltaTime;
+            }
+        }
+    } 
+    
 }
