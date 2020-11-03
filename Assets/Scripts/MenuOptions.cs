c93c3d86b170ea68c3d1f5d3129c44947cd64836
Jalen Wayt <jmw0077@mix.wvu.edu>
2020-11-03 21:25:29 +0000

    Added "Start" Button functionality to open the Mafia 1 scene when the user wishes to start

---
+++ Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Loads the game when the player chooses the "Start" menu item
+    public void PlayGame(){
+        SceneManager.LoadScene("Mafia 1");
+    }
+}
