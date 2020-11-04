7e7604d91f6e9c8ec360dbec62bbfb5fd0eab97b
Jalen Wayt <jmw0077@mix.wvu.edu>
2020-11-04 00:16:03 +0000

    Added a pause menu that stops the game. However, the user is still able to shoot bullets. Need to add a conditional from PauseMenu to control when the user can turn and/or shoot.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool IsPaused;
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(IsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    void Pause(){
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    void Resume(){
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+}
