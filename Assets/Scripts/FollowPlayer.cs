9b3334f71ddcbbd21b58ae37110e430bdb10b717
Logan Reed Bland <lrb0023@mix.wvu.edu>
2020-11-30 01:23:34 +0000

    Made changes to the camera offset to go with all levels

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -9,7 +9,7 @@ public class FollowPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        offset = new Vector3(0, 20, 0);
+        
     }
 
     // Update is called once per frame
